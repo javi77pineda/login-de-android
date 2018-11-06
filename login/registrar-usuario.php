4156b677d9a10f332ea50377651e6ff95b67bdd4
javi77pineda <jpineda0000@gmail.com>
2018-11-06 15:11:29 -0500

    Add files via upload

---
+++ login/registrar-usuario.php
@@ -0,0 +1,52 @@
+<?php
+
+ $host_db = "localhost";
+ $user_db = "root";
+ $pass_db = "";
+ $db_name = "meduca";
+ $tbl_name = "login";
+ 
+
+ $form_pass = $_POST['password'];
+
+
+ $hash = $form_pass;
+
+ $conexion = new mysqli($host_db, $user_db, $pass_db, $db_name);
+ 
+ 
+
+ if ($conexion->connect_error) {
+ die("La conexion falló: " . $conexion->connect_error);
+}
+
+ $buscarUsuario = "SELECT * FROM $tbl_name
+ WHERE usuarios = '$_POST[username]' ";
+
+ $result = $conexion->query($buscarUsuario);
+
+ $count = mysqli_num_rows($result);
+
+ if ($count == 1) {
+ echo "<br />". "El Nombre de Usuario ya a sido tomado." . "<br />";
+
+ echo "<a href='index.html'>Por favor escoga otro Nombre</a>";
+ }
+ else{
+
+ $query = "INSERT INTO login (usuarios, password)
+           VALUES ('$_POST[username]', '$hash')";
+
+ if ($conexion->query($query) === TRUE) {
+ 
+ echo "<br />" . "<h2>" . "Usuario Creado Exitosamente!" . "</h2>";
+ echo "<h4>" . "Bienvenido: " . $_POST['username'] . "</h4>" . "\n\n";
+ echo "<h5>" . "Hacer Login: " . "<a href='login.html'>Login</a>" . "</h5>"; 
+ }
+
+ else {
+ echo "Error al crear el usuario." . $query . "<br>" . $conexion->error; 
+   }
+ }
+ mysqli_close($conexion);
+?>
